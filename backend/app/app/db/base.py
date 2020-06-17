67750d3944394d862d884c976c3ecc77f8553c84
Ethan Vo <evv@alum.mit.edu>
2020-06-17 12:27:21 -1000

    new file:   backend/app/alembic/versions/13d5b7bf4214_add_chinook_models.py 	modified:   backend/app/app/db/base.py 	new file:   backend/app/app/models/chinook.py

--- backend/app/app/db/base.py
+++ backend/app/app/db/base.py
@@ -3,3 +3,4 @@
 from app.db.base_class import Base  # noqa
 from app.models.item import Item  # noqa
 from app.models.user import User  # noqa
+from app.models.chinook import Artist, Employee, Genre, MediaType, Playlist, Album, Customer, Invoice, Track, InvoiceLine
